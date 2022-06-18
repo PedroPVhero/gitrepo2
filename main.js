10f878e9d2a6a14f15ba8ef603d7463fefc5cb1a
PedroPVhero <verissimo0107@gmail.com>
2022-06-18 20:42:52 -0300

    teste

--- main.js
+++ main.js
@@ -1,9 +1,31 @@
-function doMath(){
-	// Capture the entered values of two input boxes
+function doMath(s){
         var my_input1 = document.getElementById('my_input1').value;
         var my_input2 = document.getElementById('my_input2').value;
 
-        // Add them together and display
-        var sum = parseInt(my_input1) + parseInt(my_input2);
-        document.write(sum);
+	switch(s){
+		case "soma":
+        		// Add them together and display
+        		var sum = parseInt(my_input1) + parseInt(my_input2);
+        		document.write(sum);
+			break;
+		case "multiplica":
+			var multi = parseInt(my_input1) * parseInt(my_input2);
+			document.write(multi);
+			break;
+		case "subtrai":
+			var sub = parseInt(my_input1) - parseInt(my_input2);
+			document.write(sub);
+			break;
+		case "divide":
+			var div = parseInt(my_input1) / parseInt(my_input2);
+			document.write(div);
+			break;
+		case "potencia":
+			var pow = Math.pow(parseInt(my_input1), parseInt(my_input2));
+			document.write(pow);
+			break;
+		case default:
+			document.append("Erro");
+			break
+	}
 }
